bf027a00dd1d04bebf95d2896c8c5fca1b2c95ac
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-16 15:32:55 +0900

    connected to Mongo DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongo DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
