f61025dfbe4599814d0e9b05236a76dae855b668
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-16 14:50:11 +0900

    Created routes and header component in React and Created server

---
+++ client/src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/sign-up'
+          element={<SignUp />}
+        />
+        <Route
+          path='/sign-in'
+          element={<SignIn />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/profile'
+          element={<Profile />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
