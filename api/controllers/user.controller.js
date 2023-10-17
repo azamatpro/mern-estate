cf33a9bc78ff0b09223e6108bb20099747a1df6a
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-17 10:43:20 +0900

    Created a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+  res.json({
+    message: "Api router is working!",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -16,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
