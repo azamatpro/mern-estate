3c73ec7f17a0e20f49035efcb8a4cfd92f97903e
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-18 19:47:50 +0900

    Set redux toolkit

---
+++ client/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
