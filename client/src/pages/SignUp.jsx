a99a8f0802344fd13d9d729be453ddb3bec9df72
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-18 17:29:12 +0900

    Completed sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -30,7 +30,7 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate("/sign-in");
+      navigate("/");
     } catch (error) {
       setLoading(false);
       setError(error.message);
