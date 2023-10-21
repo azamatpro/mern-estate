0514122eea707a061b0abefdd638826e2e8fd55f
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-21 19:50:55 +0900

    Made profilte private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -27,10 +28,12 @@ export default function App() {
           path='/about'
           element={<About />}
         />
-        <Route
-          path='/profile'
-          element={<Profile />}
-        />
+        <Route element={<PrivateRoute />}>
+          <Route
+            path='/profile'
+            element={<Profile />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -26,8 +28,16 @@ export default function Header() {
           <Link to={"/about"}>
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-          <Link to={"/sign-in"}>
-            <li className=' text-slate-700 hover:underline'>Sign In</li>
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                className='w-7 h-7 rounded-full object-cover'
+                src={currentUser.avatar}
+                alt='Profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
