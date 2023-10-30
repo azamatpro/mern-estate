3b2e94a575d20663440471e2270f3aa07d301695
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-30 19:44:47 +0900

    Completed create listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,7 +12,7 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
-    return next(errorHandler(401, "Listing not found!"));
+    return next(errorHandler(404, "Listing not found!"));
   }
   if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, "You can only delete your own listings!"));
@@ -20,6 +20,22 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
