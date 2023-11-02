b85b2adc0d8051c240bcd660ff20147ce9c7adc1
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-11-02 16:28:16 +0900

    Added image slider to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 export default function App() {
   return (
@@ -29,6 +30,10 @@ export default function App() {
         <Route
           path='/about'
           element={<About />}
+        />
+        <Route
+          path='/listing/:listingId'
+          element={<Listing />}
         />
         <Route element={<PrivateRoute />}>
           <Route
