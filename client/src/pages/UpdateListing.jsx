03e461a379b0e8ed61f1a68b2ba8443652ccf68d
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-11-02 17:12:27 +0900

    Completed listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -252,7 +252,7 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === "rent" && <span className='text-xs'>($ / month)</span>}
               </div>
             </div>
             {formData.offer && (
@@ -269,7 +269,7 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
+                  {formData.type === "rent" && <span className='text-xs'>($ / month)</span>}
                 </div>
               </div>
             )}
