a8bc79bcc3706d6f903525a4b48eb2f1ccd436f9
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-11-03 12:07:33 +0900

    Created search api route and completed header search form functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createListing, deleteListing, updateListing, getListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+  getListings,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
@@ -7,4 +13,5 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 export default router;
